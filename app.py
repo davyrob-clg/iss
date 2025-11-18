f1bd694349cda7b1f5bba7dbae51fd0410177bde
Dave Robertson <dave.robertson@unigib.edu.gi>
2025-11-18 13:03:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from tkintermapview import TkinterMapView
 import requests
 import time
 
+from PIL import Image, ImageTk
+
+
+
 class ISSTrackerApp:
+
+    custom_image: tk.PhotoImage
+
     def __init__(self, master):
         self.master = master
         master.title("ISS Tracker")
@@ -16,6 +23,18 @@ class ISSTrackerApp:
         self.map_widget.set_zoom(1)
 
         self.iss_marker = None
+        image_path = "iss.png"  # Replace with the actual path to your image
+        original_image = Image.open(image_path)
+        resized_image = original_image.resize((32, 32), Image.LANCZOS) # Image.LANCZOS for high-quality downsampling
+ 
+        self.custom_image = ImageTk.PhotoImage(resized_image)
+
+        
+        latitude = float(0)
+        longitude = float(0)
+
+        marker = self.map_widget.set_marker(52.5200, 13.4050, icon=self.custom_image)
+
         self.update_iss_location()
 
     def get_iss_location(self):
@@ -31,10 +50,12 @@ class ISSTrackerApp:
 
     def update_iss_location(self):
         latitude, longitude = self.get_iss_location()
-        if latitude is not None and longitude is not None:
+        if latitude is not None and longitude is not None:            
+            
             if self.iss_marker:
                 self.map_widget.delete(self.iss_marker) # Remove previous marker
-            self.iss_marker = self.map_widget.set_marker(latitude, longitude, text="ISS")
+            
+            self.iss_marker = self.map_widget.set_marker(latitude, longitude, icon=self.custom_image)
         
         self.master.after(5000, self.update_iss_location) # Update every 5 seconds
 
